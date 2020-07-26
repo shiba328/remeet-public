6008205311273b2d0fdb7328e8c60f170abad5cb
shiba mitsue <mitue.okamoto@gmail.com>
2020-07-27 03:25:03 +0900

    fix

---
+++ src/support/app-in-browser.md
@@ -0,0 +1,27 @@
+---
+name: app-in-browser
+title: アプリ内ブラウザを利用することは可能ですか？
+parent: support
+---
+
+一部のアプリにおいて、全部または一部の機能が利用可能ですが、原則原則リミートライブでは以下を推奨します。
+
+- iOS
+  - Safari 安定版の最新2バージョン
+- Android
+  - Google Chrome 安定版の最新2バージョン
+  - ※ただし、Androidは端末依存により利用できない場合があります。確実に動作させたい場合にはAndroid SDKでの実装を推奨いたします。
+- PC
+  - Google Chrome 安定版の最新2バージョン
+  - Firefox 安定版の最新2バージョン
+  - Safari 安定版の最新2バージョン
+  - Microsoft Edge 安定版の最新2バージョン
+
+## 各アプリで開いた場合の対処方法
+
+- ツイッターアプリから開いた場合
+  画面右下のSafariボタンを押してください
+
+- FACEBOOKアプリから開いた場合
+
+- Microsoft Teamsで開いた場合
--- webpack.config.js
+++ webpack.config.js
@@ -1,128 +1,210 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { GenerateSW } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: `./src/index.pug`,
-			filename: `index.html`,
-			minify: {
-				collapseWhitespace: true,
-				removeComments: true,
-				removeRedundantAttributes: true,
-				removeScriptTypeAttributes: true,
-				removeStyleLinkTypeAttributes: true,
-				useShortDoctype: true
-			}
-		}),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/assets/public/'),
-        to: path.resolve(__dirname, 'dist')
-      },
-    ]),
-		new webpack.ProgressPlugin(),
-		new MiniCssExtractPlugin()
-	],
-	module: {
-		rules: [
-			//js
-			{
-				test: /.(js|jsx)$/,
-				include: [],
-				loader: 'babel-loader'
-			},
-			// html
-			{
-				test: /\.html$/,
-				use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-			},
-			// pug
-			{
-        test: /\.pug$/,
-        use: [
-          "html-loader",
-          {
-            loader: "pug-html-loader",
-            // options: {
-            //   pretty: true,
-            //   minimize: false,
-            //   basedir: path.join(__dirname, 'src')
-            // }
-          }
-        ]
-			},
-			// css
-			{
-				test: /.css$/,
-				use: [
-					// {
-					// 	loader: MiniCssExtractPlugin.loader
-					// },
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1,
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							plugins: function() {
-								return [
-									require('precss'),
-									require('autoprefixer'),
-									require('postcss-nested'),
-									require('postcss-custom-media')
-								];
-							}
-						}
-					}
-				]
-			},
-			// img
-			{
-        test: /\.(jpg|png)$/,
-        use: [
-          {
-            loader: 'file-loader',
+const markdownIt = require('markdown-it');
+const prism = require('markdown-it-prism');
+const attrs = require('markdown-it-attrs');
+const mermaid = require('markdown-it-mermaid');
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (env, argv) => {
+
+  const MyPageLoad = (inputDir, outputDir, layouts, mode) => {
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, inputDir));
+
+    console.log(templateFiles)
+
+    return templateFiles.map(item => {
+      const parts = item.split('.');
+      const name = parts[0];
+      const ext = parts[1];
+      
+      let path = `${outputDir}${name}`;
+      let template = `${inputDir}/${name}.${ext}`;
+      let filename = `${path}/index.html`;
+      let url =  `/${path}/`;
+      let templateName = "";
+      const srcDir = inputDir.split('/').slice(-1)[0];
+      
+      if(layouts) {
+        template = layouts;
+      }
+      
+      console.log(template, filename, path)
+      return new HtmlWebPackPlugin({
+        template: template,
+        filename: filename,
+        env: {
+          srcDir: srcDir,
+          path: path, 
+          item: inputDir,
+          name: name,
+          root: `/${outputDir}`, 
+          url: url,
+          mode: argv.mode
+        },
+        minify: {
+          collapseWhitespace: argv.mode === 'development',
+          removeComments: argv.mode === 'development',
+          removeRedundantAttributes: argv.mode === 'development',
+          removeScriptTypeAttributes: argv.mode === 'development',
+          removeStyleLinkTypeAttributes: argv.mode === 'development',
+          useShortDoctype: argv.mode === 'development'
+        },
+        inject: argv.mode == 'development' ? true : false
+      });
+    });
+  }
+
+  const postcssPlugins = () => {
+    return [
+      require('stylelint'),
+      require('precss'),
+      require('autoprefixer'),
+      require('postcss-nested'),
+      require('postcss-custom-media'),
+      require('postcss-each')
+    ];
+  }
+
+  return {
+    entry: `./src/${argv.mode}.index.js`,
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/dist'
+    },
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, '/src'),
+        '@': path.join(__dirname, '/'),
+      }
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.join(__dirname, 'src/index.pug'),
+        minify: {
+          collapseWhitespace: argv.mode === 'development',
+          removeComments: argv.mode === 'development',
+          removeRedundantAttributes: argv.mode === 'development',
+          removeScriptTypeAttributes: argv.mode === 'development',
+          removeStyleLinkTypeAttributes: argv.mode === 'development',
+          useShortDoctype: argv.mode === 'development'
+        }
+      }),
+      // new GenerateSW(),
+      new MiniCssExtractPlugin({
+        // filename: "assets/css/style.css"
+      }),
+      new CopyPlugin([
+        {
+          from: path.resolve(__dirname, 'src/assets/public'),
+          to: path.resolve(__dirname, 'dist/')
+        }
+      ]),
+      // new CopyPlugin([
+      //   {
+      //     from: path.resolve(__dirname, 'src/ads.txt'),
+      //     to: path.resolve(__dirname, 'dist/ads.txt')
+      //   }
+      // ])
+    ]
+      .concat(MyPageLoad('./src/support', 'support/', './src/layouts/support.pug'))
+      // .concat(MyPageLoad('./src/support/svg', 'svg/', './src/layouts/support.pug'))
+      // .concat(MyPageLoad('./src/support/webpack-media', 'webpack-media/', './src/layouts/support.pug'))
+      // .concat(DemoPageLoad('./src/page'))
+
+      .concat(new HtmlWebPackPlugin({
+        template: path.join(__dirname, 'src/support.pug'),
+        filename: 'support/index.html',
+        minify: {
+          collapseWhitespace: argv.mode === 'development',
+          removeComments: argv.mode === 'development',
+          removeRedundantAttributes: argv.mode === 'development',
+          removeScriptTypeAttributes: argv.mode === 'development',
+          removeStyleLinkTypeAttributes: argv.mode === 'development',
+          useShortDoctype: argv.mode === 'development'
+        }
+      })),
+      // .concat(new HTMLInlineCSSWebpackPlugin()),
+    module: {
+      rules: [
+        // html
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: argv.mode !== 'development'
+              }
+            }
+          ]
+        },
+        // css
+        {
+          test: /\.?(p|)css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                sourceMap: argv.mode === 'development'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: postcssPlugins
+              }
+            }
+          ]
+        },
+        // md
+        {
+          test: /\.md$/,
+          use: [
+            {
+              loader: 'frontmatter-markdown-loader',
+              options: {
+                markdownIt: markdownIt()
+                .use(mermaid.default)
+                .use(prism, { defaultLanguage: 'shell' })
+                .use(attrs, {})
+              }
+            }
+          ]
+        },
+        // pug
+        {
+          test: /\.pug$/,
+          use: {
+            loader: 'pug-loader',
             options: {
-							name: '/images/[name].[ext]',
+              root: path.resolve(__dirname, 'src')
             }
           }
-        ]
-      }
-		]
-	},
-
-	optimization: {
-		minimizer: [new TerserPlugin()],
-
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
-};
+        },
+        //svg
+        {
+          test: /\.svg$/,
+          loader: 'svg-inline-loader?classPrefix'
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new OptimizeCSSAssetsPlugin()
+      ]
+    }
+  }
+}
